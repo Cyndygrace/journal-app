f174a92d53f63a4b2460115b6eb18480e7b5f7db
cynthia iloekwe <cyniloekwe@gmail.com>
2019-08-17 21:36:31 +0100

    added user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
-import express from 'express';
-const router = express.Router();
+import {Router} from 'express';
+import users from './users/usersRoute';
+const router = Router();
 
 /* GET home page. */
-router.get('/', function(_req: express.Request, res: express.Response) {
-  res.send({message: 'this is home'})
+router.get('/', function(_req, res) {
+  res.status(200).json({message:'this is home'})
 });
+
+router.use('/users', users);
 
 export default router;
